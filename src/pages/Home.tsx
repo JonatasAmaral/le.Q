cc09f7b0c4cad8c322d4360531b98139a9eeced3
Jonatas Amaral <jonataspadtab@gmail.com>
2021-06-23 20:49:56 -0300

    make raw navigation across pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom';
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import loginIcon from "../assets/images/log-in.svg";
@@ -6,7 +8,14 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
-export function Home() {return (
+export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new');
+  }
+  
+return (
   <div id="page-auth">
     <aside>
       <img
@@ -20,7 +29,7 @@ export function Home() {return (
       <div className="main-content">
         <img src={logoImg} alt="logo letmeask" />
 
-        <button className="create-room">
+        <button className="create-room" onClick={navigateToNewRoom} >
           <img src={googleIconImg} alt="logo da Google" />
           Crie sua sala com o Google
         </button>
