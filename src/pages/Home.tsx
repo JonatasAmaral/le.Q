2bd29009f31128b2a5b70d757a59505772febd3a
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-04 01:12:24 -0300

    make existing room's page access from home by ID

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
@@ -6,16 +7,33 @@ import logoImg from "../assets/images/logo.svg";
 import loginIcon from "../assets/images/log-in.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import { database } from '../services/firebase';
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
 export function Home() {
   const history = useHistory();
   const {user, signInWithGoogle} = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) await signInWithGoogle();
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    // if (roomCode.trim() === '') return;
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()) {
+      alert("Room doesn't exists");
+      return;
+    }
+
+    history.push(`/room/${roomCode}`);
   }
   
 return (
@@ -32,15 +50,20 @@ return (
       <div className="main-content">
         <img src={logoImg} alt="logo letmeask" />
 
-        <button className="create-room" onClick={handleCreateRoom} >
+        <button className="create-room" onClick={handleCreateRoom}>
           <img src={googleIconImg} alt="logo da Google" />
           Crie sua sala com o Google
         </button>
 
         <span className="separator">ou entre em uma sala</span>
 
-        <form action="">
-          <input type="text" placeholder="Digite o código da sala" />
+        <form onSubmit={handleJoinRoom}>
+          <input
+            type="text"
+            placeholder="Digite o código da sala"
+            onChange={(e) => setRoomCode(e.target.value)}
+            value={roomCode}
+          />
           <Button type="submit">
             <img src={loginIcon} alt="Icone representando login" />
             Entrar na sala
