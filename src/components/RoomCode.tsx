76dae44e347553d9779c1d6623003b7a0e71b4cb
Jonatas Amaral <jonataspadtab@gmail.com>
2021-09-04 15:25:59 -0300

    create copyRoomCode button

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,25 @@
+import copyimg from "../assets/images/copy.svg";
+
+import '../styles/room-code.scss'
+
+type RoomCodeProps = {
+  roomCode: string;
+}
+
+export function RoomCode( {roomCode}: RoomCodeProps) {
+
+  function copyRoomCodeToClipboard(){
+    navigator.clipboard.writeText(roomCode)
+  }
+  return (
+    <button
+      className="room-code"
+      onClick={copyRoomCodeToClipboard}
+    >
+      <div>
+        <img src={copyimg} alt="Copiar código da sala" />
+      </div>
+      <span>Sala #{roomCode}</span>
+    </button>
+  );
+}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,6 @@
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
 import "../styles/room.scss";
 
 export function Room() {
@@ -10,7 +11,7 @@ export function Room() {
       <header>
         <div className="content">
           <img src={logoImg} alt="" />
-          <div>codigo</div>
+          <RoomCode roomCode={'65161361313'} />
         </div>
       </header>
 
